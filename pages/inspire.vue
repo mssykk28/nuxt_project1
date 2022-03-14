6ba15e22cdb5b597034db0242a06c76e4ab43cb8
masashi <mss.nt.2828@gmail.com>
2022-03-14 13:08:08 +0900

    Vuetifyの試し

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,21 +1,83 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <v-treeview :items="items"></v-treeview>
 </template>
 
 <script>
 export default {
   name: 'InspirePage',
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        name: 'Applications :',
+        children: [
+          { id: 2, name: 'Calendar : app' },
+          { id: 3, name: 'Chrome : app' },
+          { id: 4, name: 'Webstorm : app' },
+        ],
+      },
+      {
+        id: 5,
+        name: 'Documents :',
+        children: [
+          {
+            id: 6,
+            name: 'vuetify :',
+            children: [
+              {
+                id: 7,
+                name: 'src :',
+                children: [
+                  { id: 8, name: 'index : ts' },
+                  { id: 9, name: 'bootstrap : ts' },
+                ],
+              },
+            ],
+          },
+          {
+            id: 10,
+            name: 'material2 :',
+            children: [
+              {
+                id: 11,
+                name: 'src :',
+                children: [
+                  { id: 12, name: 'v-btn : ts' },
+                  { id: 13, name: 'v-card : ts' },
+                  { id: 14, name: 'v-window : ts' },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 15,
+        name: 'Downloads :',
+        children: [
+          { id: 16, name: 'October : pdf' },
+          { id: 17, name: 'November : pdf' },
+          { id: 18, name: 'Tutorial : html' },
+        ],
+      },
+      {
+        id: 19,
+        name: 'Videos :',
+        children: [
+          {
+            id: 20,
+            name: 'Tutorials :',
+            children: [
+              { id: 21, name: 'Basic layouts : mp4' },
+              { id: 22, name: 'Advanced techniques : mp4' },
+              { id: 23, name: 'All about app : dir' },
+            ],
+          },
+          { id: 24, name: 'Intro : mov' },
+          { id: 25, name: 'Conference introduction : avi' },
+        ],
+      },
+    ],
+  }),
 }
 </script>
